18b9ebaeb768acaf8300f6cafb3f2a888a2b5fc8
Abhishek Wanve <81671930+war02abhishek@users.noreply.github.com>
2023-07-14 00:01:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = express();
 dotenv.config();
 
 
-import userRoutes from './api/routes/users.js'
-import questionRoutes from './api/routes/Questions.js'
-import answerRoutes from './api/routes/Answers.js'
-import qcommentRoutes from './api/routes/qcomments.js'
-import acommentRoutes from './api/routes/acomments.js'
+import userRoutes from './routes/users.js'
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
+import qcommentRoutes from './routes/qcomments.js'
+import acommentRoutes from './routes/acomments.js'
 
 
 app.use(express.json({limit: "30mb", extended: true}))
@@ -25,10 +25,10 @@ app.get('/',(req, res) => {
     res.send("This is a stack overflow clone API")
 });
 
-app.use('/user', userRoutes)
-app.use('/questions', questionRoutes)
-app.use('/answer', answerRoutes)
-app.use('/qcomment', qcommentRoutes)
-app.use('/acomment', acommentRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/questions', questionRoutes)
+app.use('/api/answer', answerRoutes)
+app.use('/api/qcomment', qcommentRoutes)
+app.use('/api/acomment', acommentRoutes)
 
 export default app;
